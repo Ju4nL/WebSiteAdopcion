834f0b60eadf848fd9512a7ff3ba6c9a4dad77d4
Juan Lozada <juanlozadacochaches@gmail.com>
2024-11-24 14:37:23 -0500

    update

--- js/main.js
+++ js/main.js
@@ -5,6 +5,18 @@ function toggleMenu() {
     const navMenu = document.querySelector('.nav-menu');
     const navMenuBg = document.querySelector('.nav-menu-gb');
 
+    const closeMenu = () => {
+        navMenu.addEventListener(
+            'transitionend',
+            () => {
+                body.classList.remove('menu-active'); // Oculta el menú y el fondo
+                navMenuBg.style.right = '-2000rem'; // Desaparece el fondo sin transición
+            },
+            { once: true }
+        );
+        navMenu.style.right = '-20rem'; // Inicia la transición del menú
+    };
+
     if (burgerButton) {
         burgerButton.addEventListener('click', () => {
             body.classList.add('menu-active'); // Abre el menú y muestra el fondo
@@ -14,22 +26,57 @@ function toggleMenu() {
     }
 
     if (closeButton) {
-        closeButton.addEventListener('click', () => {
-            navMenu.addEventListener(
-                'transitionend',
-                () => {
-                    body.classList.remove('menu-active'); // Oculta el menú y el fondo
-                    navMenuBg.style.right = '-2000rem'; // Desaparece el fondo sin transición
-                },
-                { once: true }  
-            );
-            navMenu.style.right = '-20rem'; // Inicia la transición del menú
+        closeButton.addEventListener('click', closeMenu);
+    }
+
+    if (navMenuBg) {
+        navMenuBg.addEventListener('click', () => {
+            if (window.innerWidth <= 1024) { // Condición para pantallas menores o iguales al tamaño tablet
+                closeMenu();
+            }
         });
     }
 }
 
+function initCarousel() {
+    let currentIndex = 0;
+
+    const showSlide = (index) => {
+        const carouselImages = document.querySelector('.carousel-images');
+        const totalSlides = document.querySelectorAll('.carousel-images img').length;
+
+        if (index >= totalSlides) {
+            currentIndex = 0; // Regresar al inicio
+        } else if (index < 0) {
+            currentIndex = totalSlides - 1; // Ir al final
+        } else {
+            currentIndex = index;
+        }
+
+        const offset = -currentIndex * 100;
+        carouselImages.style.transform = `translateX(${offset}%)`;
+    };
+
+    const nextSlide = () => {
+        showSlide(currentIndex + 1);
+    };
+
+    const prevSlide = () => {
+        showSlide(currentIndex - 1);
+    };
+
+    // Cambiar imagen automáticamente cada 3 segundos
+    setInterval(() => {
+        nextSlide();
+    }, 3000);
+
+    // Mostrar la primera imagen al cargar la página
+    showSlide(currentIndex);
+}
+
 function app() {
     toggleMenu();
+    initCarousel(); // Inicializar el carrusel
 }
 
 document.addEventListener('DOMContentLoaded', app);
